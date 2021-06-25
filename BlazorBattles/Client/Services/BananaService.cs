3954322d5121c9bbe2ad547bf85a5af2e04d58b3
Tihomir Ciric <ticaspeedy@gmail.com>
2021-06-25 18:30:27 +0200

    Changed unit cost to Bananas

---
+++ BlazorBattles/Client/Services/BananaService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorBattles.Client.Services
+{
+    public class BananaService : IBananaService
+    {
+
+        public event Action OnChange;
+        public int Bananas { get; set; } = 1000;
+
+       
+
+        public void SpendBananas(int amount)
+        {
+            Bananas -= amount;
+            BananasChanged();
+
+        }
+
+        void BananasChanged() => OnChange.Invoke();
+
+        public void AddBananas(int amount)
+        {
+            Bananas += amount;
+            BananasChanged();
+        }
+    }
+}
---
+++ BlazorBattles/Shared/UserLogin.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorBattles.Shared
+{
+   public class UserLogin
+    {
+        [Required(ErrorMessage ="Please enter a username")]
+        public string UserName { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
